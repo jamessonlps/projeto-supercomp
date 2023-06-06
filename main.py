de7fe734b9d57ef394239b7b9e5753e80668510a
jamessonlps <jamesson1233@gmail.com>
2023-06-06 13:46:47 -0300

    add openmp script

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ class Knapsack():
     self.results_t1_aleatoria = None # resultados experimento t1 para aleatória
     self.results_t2_aleatoria = None # resultados experimento t2 para aleatória
 
+    self.results_t1_openmp = None # resultados experimento t1 para openmp
+    self.results_t2_openmp = None # resultados experimento t2 para openmp
+
 
   def generate_input_files(self) -> None:
     """
@@ -109,6 +112,38 @@ class Knapsack():
         file.write(proc.stdout)
       i += 1
 
+  
+  def _generate_openmp_outputs(self):
+    # Gera resultados do teste 1 => Número de filmes é fixo
+    list_files_t1 = os.listdir("./inputs/test1")
+    i = 0
+    for input_file in list_files_t1:
+      with open(f"./inputs/test1/{input_file}") as file:
+        proc = subprocess.run(
+          ['./openmp', f'./outputs/openmp/test1/openmp-out-{i}.json'],
+          input=file.read(),
+          text=True,
+          capture_output=True
+        )
+      with open(f"./outputs/logs/openmp-test1-{i}.txt", "w+") as file:
+        file.write(proc.stdout)
+      i += 1
+    
+    # Gera resultados do teste 2 => Número de categorias é fixo
+    list_files_t2 = os.listdir("./inputs/test2")
+    i = 0
+    for input_file in list_files_t2:
+      with open(f"./inputs/test2/{input_file}") as file:
+        proc = subprocess.run(
+          ['./openmp', f'./outputs/openmp/test2/openmp-out-{i}.json'],
+          input=file.read(),
+          text=True,
+          capture_output=True
+        )
+      with open(f"./outputs/logs/openmp-test2-{i}.txt", "w+") as file:
+        file.write(proc.stdout)
+      i += 1
+
 
   def _get_results_gulosa(self):
     self._generate_gulosa_outputs()
@@ -192,6 +227,48 @@ class Knapsack():
         results["num_categories"].append(data["num_categories"])
         results["selected"].append(data["selected"])
     self.results_t2_aleatoria = results
+
+
+  def _get_results_openmp(self):
+    self._generate_openmp_outputs()
+
+    # Resultados para o teste 1
+    files_json_openmp_t1 = os.listdir("./outputs/openmp/test1")
+    results = {
+      "exec_time": [], 
+      "screen_time": [], 
+      "num_movies": [], 
+      "num_categories": [],
+      "num_threads": []
+    }
+    for file in files_json_openmp_t1:
+      with open(f"./outputs/openmp/test1/{file}", "r") as f:
+        data = json.load(f)
+        results["exec_time"].append(data["exec_time"] / 1000000)
+        results["screen_time"].append(data["screen_time"])
+        results["num_movies"].append(data["num_movies"])
+        results["num_categories"].append(data["num_categories"])
+        results["num_threads"].append(data["num_threads"])
+    self.results_t1_openmp = results
+
+    # Resultados para o teste 2
+    files_json_openmp_t2 = os.listdir("./outputs/openmp/test2")
+    results = {
+      "exec_time": [], 
+      "screen_time": [], 
+      "num_movies": [], 
+      "num_categories": [],
+      "num_threads": []
+    }
+    for file in files_json_openmp_t2:
+      with open(f"./outputs/openmp/test2/{file}", "r") as f:
+        data = json.load(f)
+        results["exec_time"].append(data["exec_time"] / 1000000)
+        results["screen_time"].append(data["screen_time"])
+        results["num_movies"].append(data["num_movies"])
+        results["num_categories"].append(data["num_categories"])
+        results["num_threads"].append(data["num_threads"])
+    self.results_t2_openmp = results
 
 
   def plot_results_gulosa(self):
@@ -299,6 +376,59 @@ class Knapsack():
     plt.clf()
   
 
+  def plot_results_openmp(self):
+    self._get_results_openmp()
+
+    plt.scatter(self.results_t1_openmp["num_categories"], self.results_t1_openmp["exec_time"], c="r", label="Aleatória")
+    plt.xlabel("Número de categorias")
+    plt.ylabel("Tempo de execução do algoritmo [s]")
+    plt.title(f"Número de filmes fixo: {self.fix_movie}")
+    plt.grid(True)
+    plt.savefig("./img/openmp-filmes-fixo.png")
+    plt.clf()
+
+    plt.scatter(self.results_t2_openmp["num_movies"], self.results_t2_openmp["exec_time"], c="r", label="Aleatória")
+    plt.xlabel("Número de filmes")
+    plt.ylabel("Tempo de execução do algoritmo [s]")
+    plt.title(f"Número de categorias fixo: {self.fix_category}")
+    plt.grid(True)
+    plt.savefig("./img/openmp-categ-fixo.png")
+    plt.clf()
+
+    plt.scatter(self.results_t1_openmp["num_categories"], self.results_t1_openmp["screen_time"], c="r", label="Aleatória")
+    plt.xlabel("Número de categorias")
+    plt.ylabel("Tempo de tela [h]")
+    plt.title(f"Número de filmes fixo: {self.fix_movie}")
+    plt.grid(True)
+    plt.savefig("./img/openmp-filmes-fixo-tela.png")
+    plt.clf()
+
+    plt.scatter(self.results_t2_openmp["num_movies"], self.results_t2_openmp["screen_time"], c="r", label="Aleatória")
+    plt.xlabel("Número de filmes")
+    plt.ylabel("Tempo de tela [h]")
+    plt.title(f"Número de categorias fixo: {self.fix_category}")
+    plt.grid(True)
+    plt.savefig("./img/openmp-categ-fixo-tela.png")
+    plt.clf()
+
+    plt.scatter(self.results_t1_openmp["num_categories"], self.results_t1_openmp["selected"], c="r", label="Aleatória")
+    plt.xlabel("Número de categorias")
+    plt.ylabel("Número de filmes selecionados")
+    plt.title(f"Número de filmes fixo: {self.fix_movie}") 
+    plt.grid(True)
+    plt.savefig("./img/openmp-filmes-fixo-selecionados.png")
+    plt.clf()
+
+    plt.scatter(self.results_t2_openmp["num_movies"], self.results_t2_openmp["selected"], c="r", label="Aleatória")
+    plt.xlabel("Número de filmes")
+    plt.ylabel("Número de filmes selecionados")
+    plt.title(f"Número de categorias fixo: {self.fix_category}")
+    plt.grid(True)
+    plt.savefig("./img/openmp-categ-fixo-selecionados.png")
+    plt.clf()
+
+
+
   def plot_results_comparison(self):
     plt.scatter(self.results_t1_aleatoria["num_categories"], self.results_t1_aleatoria["exec_time"], c="r", label="Aleatória")
     plt.scatter(self.results_t1_gulosa["num_categories"], self.results_t1_gulosa["exec_time"], c="g", label="Gulosa")
@@ -361,9 +491,10 @@ class Knapsack():
   def run(self):
     # self.generate_input_files()
 
-    self.plot_results_gulosa()
-    self.plot_results_aleatoria()
-    self.plot_results_comparison()
+    # self.plot_results_gulosa()
+    # self.plot_results_aleatoria()
+    self.plot_results_openmp()
+    # self.plot_results_comparison()
 
 
 if __name__ == "__main__":
--- openmp.cpp
+++ openmp.cpp
@@ -14,6 +14,11 @@ struct movie_item {
   bitset<24> time_used;
 };
 
+struct exhaustive_return {
+  bitset<64> solution;
+  int screen_time;
+};
+
 void fill_movie_time_used(movie_item &movie_item) {
   for (int i = 0; i < 24; i++) {
     if (movie_item.m.begin < movie_item.m.end) {
@@ -28,10 +33,10 @@ void fill_movie_time_used(movie_item &movie_item) {
 }
 
 
-vector<bitset<64>> exhaustive(vector<movie_item> &mis, map<int, int> &max_by_cat, int num_movies, int num_threads) {
-  long int max_solutions = pow(2, num_movies);
+vector<exhaustive_return> exhaustive(vector<movie_item> &mis, map<int, int> &max_by_cat, int num_movies, int num_threads) {
+  long int max_solutions = min(pow(2, num_movies), pow(2, 62));
 
-  vector<bitset<64>> solutions;
+  vector<exhaustive_return> solutions;
 
   #pragma omp parallel
   {
@@ -40,27 +45,25 @@ vector<bitset<64>> exhaustive(vector<movie_item> &mis, map<int, int> &max_by_cat
       map<int, int> max_by_cat_copy = max_by_cat;
       bitset<64> solution(i);
       bitset<24> time_available;
+      int screen_time = 0;
 
       for (int j = 0; j < num_movies; j++) {
         if (solution[j] == 1) {
           bitset<24> is_addable = mis[j].time_used & time_available;
 
-          if (is_addable != 0)
-            break;
-
-          if (max_by_cat_copy[mis[j].m.category] == 0)
-            break;
-
-          time_available = time_available | mis[j].time_used;
-          max_by_cat_copy[mis[j].m.category] -= 1;
-        }
-
-        if (j == num_movies - 1) {
-          #pragma omp critical
-          {
-            solutions.push_back(solution);
+          if ((is_addable != 0) || (max_by_cat_copy[mis[j].m.category] == 0))
+            continue;
+          else {
+            time_available = time_available | mis[j].time_used;
+            max_by_cat_copy[mis[j].m.category] -= 1;
+            screen_time += mis[j].m.duration;
           }
         }
+      }
+      
+      #pragma omp critical
+      {
+        solutions.push_back({ solution, screen_time });
       }
     }
   }
@@ -75,7 +78,7 @@ int main(int argc, char *argv[]) {
 
   int num_movies;
   int num_categories;
-  int num_threads = omp_get_max_threads();
+  int num_threads = 4;
 
   cin >> num_movies;
   cin >> num_categories;
@@ -96,7 +99,7 @@ int main(int argc, char *argv[]) {
 
   auto start_exec = chrono::high_resolution_clock::now();
 
-  vector<bitset<64>> solutions = exhaustive(
+  vector<exhaustive_return> solutions_response = exhaustive(
     movies_item, 
     max_by_category, 
     num_movies, 
@@ -104,40 +107,31 @@ int main(int argc, char *argv[]) {
   );
 
   auto end_exec = chrono::high_resolution_clock::now();
-
   int exec_time = chrono::duration_cast<chrono::milliseconds>(end_exec - start_exec).count();
 
   int max_solution = 0;
-  int max_solution_index = 0;
-  int num_solutions = solutions.size();
-  
+  int screen_time = 0;
+  int num_solutions = solutions_response.size();
+
+  // Obtem o indice do elemento com o maior numero de bits setados
   for (int i = 0; i < num_solutions; i++) {
-    int count = solutions[i].count();
+    int count = solutions_response[i].solution.count();
     if (count > max_solution) {
       max_solution = count;
-      max_solution_index = i;
+      screen_time = solutions_response[i].screen_time;
     }
   }
 
-  vector<movie> best_solution;
-  int duration_best_solution = 0;
-  
-  for (int i = 0; i < num_movies; i++) {
-    if (solutions[max_solution_index][i] == 1) {
-      best_solution.push_back(movies[i]);
-      duration_best_solution += movies[i].duration;
-    }
-  }
+  cout << "Tempo de execução: " << exec_time << " ms" << endl;
+  cout << "Tempo total de tela: " << screen_time << endl;
+  cout << "Número de filmes: " << num_movies << endl;
+  cout << "Número de threads: " << num_threads << endl;
 
-  cout << "Tempo de execução: " << (double) exec_time / 1000 << " s" << endl;
-  cout << "Tempo total de tela: " << duration_best_solution << endl;
-  cout << "Filmes: " << endl;
-  for (auto m : best_solution) {
-    cout << m.id << endl;
-  }
-
-  // output["solution"] = solution;
-  // output["max_solution"] = max_solution;
+  // output["exec_time"] = exec_time;
+  // output["screen_time"] = screen_time;
+  // output["num_movies"] = num_movies;
+  // output["num_categories"] = num_categories;
+  // output["num_threads"] = num_threads;
 
   // ofstream out(filename);
   // out << output.dump(2);
